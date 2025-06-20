ef5caf3f79dda213231442df211039fbcdbadaf7
Kunal verma <kunal33grd@gmail.com>
2025-06-20 17:42:56 +0530

    img urls and search functionality

--- Backend/data.js
+++ Backend/data.js
@@ -1,4 +1,4 @@
 export let products = [
-    { id: 1, name: "T-shirt", price: 299, imageUrl: "https://via.placeholder.com/150" },
-    { id: 2, name: "Cap", price: 149, imageUrl: "https://via.placeholder.com/150" }
+    { id: 1, name: "T-shirt", price: 299, imageUrl: "https://onesciencenutrition.in/wp-content/uploads/2023/10/WhatsAppImage2023-07-15at3.36.31PM-1.webp" },
+    { id: 2, name: "Cap", price: 149, imageUrl: "https://rukminim2.flixcart.com/image/850/1000/xif0q/cap/t/7/b/free-latest-ny-baseball-cap-highever-original-imagnm8yxxvmhmdy.jpeg?q=90&crop=false" }
   ];
--- Frontend/app/products/page.js
+++ Frontend/app/products/page.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 
 export default function ProductsPage() {
   const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState('');
 
   // fetch product list from backend
   useEffect(() => {
@@ -13,16 +14,32 @@ export default function ProductsPage() {
       .catch(err => console.error('Error fetching products:', err));
   }, []);
 
+   // Filtered products
+   const filtered = products.filter(product =>
+    product.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="min-h-screen bg-gray-200 p-6">
       <div className="max-w-6xl mx-auto">
         <h1 className="text-3xl font-extrabold text-black mb-6 text-center">All Products</h1>
 
-        {products.length === 0 ? (
-          <p className="text-gray-600 text-lg text-center">No products available right now.</p>
+        {/*search input */}
+        <div className="mb-6 flex justify-center">
+          <input
+            type="text"
+            placeholder="Search products..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="w-full sm:w-2/3 md:w-1/2 p-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+        </div>
+
+        {filtered.length === 0 ? (
+          <p className="text-gray-600 text-lg text-center">No Matching products found.</p>
         ) : (
           <div className="flex flex-wrap gap-6 justify-center">
-            {products.map(product => (
+            {filtered.map(product => (
               <div
                 key={product.id}
                 className="bg-white p-4 rounded-sm shadow w-full sm:w-[48%] md:w-[30%] flex flex-col items-center"
@@ -30,7 +47,7 @@ export default function ProductsPage() {
                 <img
                   src={product.imageUrl}
                   alt={product.name}
-                  className="w-full h-40 object-cover rounded-md mb-3"
+                  className="w-full h-60 object-cover rounded-lg shadow-sm mb-3"
                 />
                 <h2 className="text-lg font-semibold text-center">{product.name}</h2>
                 <p className="text-green-700 font-medium text-center">₹ {product.price}</p>
